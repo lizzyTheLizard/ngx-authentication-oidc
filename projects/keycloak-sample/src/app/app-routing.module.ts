6008e265f77005f97864ffa61a7dd183191c2d57
lizzyTheLizard <14069652+lizzyTheLizard@users.noreply.github.com>
2022-01-07 17:51:21 +0100

    Fixed smaler issues

--- projects/keycloak-sample/src/app/app-routing.module.ts
+++ projects/keycloak-sample/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ import { Public2Page } from './pages/public2/public2.page';
 const routes: Routes = [
   { path: 'public', component: PublicPage },
   { path: 'public2', component: Public2Page },
-  { path: 'auth-error', component: AuthErrorPage },
+  { path: 'auth/error', component: AuthErrorPage },
   { path: 'private', component: PrivatePage },
   { path: '', component: PublicPage },
   { path: '**', component: NotFoundPage },
